fc5b32eff6027f6698c1b9cf7a15870f55d1f69c
Rob Ross <rob@Robs-iMac.local>
2018-07-01 10:08:59 -0400

    convert Square to function component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-     render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
+function Square(props) {
+    return (
+        <button className="square" onClick={() => props.onClick()}>
+            {props.value}
         </button>
-      );
-    }
+    );
   }
   
   class Board extends React.Component {
@@ -32,10 +27,7 @@ class Square extends React.Component {
 
     renderSquare(i) {
       return (
-        <Square 
-            value={this.state.squares[i]} 
-            onClick={() => this.handleClick(i)}
-        />
+        <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
       );;
     }
   
