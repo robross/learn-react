e70fea8618ca052bf4741ecfcc1accb88be91752
Rob Ross <rob@Robs-iMac.local>
2018-07-01 11:12:06 -0400

    fix compiler warning

--- tutorial/src/board.js
+++ tutorial/src/board.js
@@ -7,7 +7,7 @@ export default class Board extends React.Component {
     renderSquare(i) {
         return (
         <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
-        );;
+        );
     }   
 
     render() {
